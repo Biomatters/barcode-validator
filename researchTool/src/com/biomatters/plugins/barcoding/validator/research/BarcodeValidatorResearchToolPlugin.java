856e37294ab73154e0e0ba3061114a0c7dc4ae0a
Matthew Cheung <matthew@biomatters.com>
2014-08-18 10:50:22 +1200

    BV-4 Mock up for research tool Commit some files to create modules for validation and the research tool

---
+++ researchTool/src/com/biomatters/plugins/barcoding/validator/research/BarcodeValidatorResearchToolPlugin.java
@@ -0,0 +1,8 @@
+package com.biomatters.plugins.barcoding.validator.research;
+
+/**
+ * @author Matthew Cheung
+ *         Created on 18/08/14 10:46 AM
+ */
+public class BarcodeValidatorResearchToolPlugin {
+}
---
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/PlaceHolder.java
@@ -0,0 +1,8 @@
+package com.biomatters.plugins.barcoding.validator.validation;
+
+/**
+ * @author Matthew Cheung
+ *         Created on 18/08/14 10:49 AM
+ */
+public class PlaceHolder {
+}
