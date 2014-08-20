6aacbd33559c18fcdfef848f0be2a339162c87ef
Gen Li <gen@biomatters.com>
2014-08-20 17:01:34 +1200

    BV-10 - Make build and tests run in TeamCity
    - Add test.
    - Add test task.

--- researchTool/test/com/biomatters/plugins/barcoding/validator/research/BarcodeValidatorTest.java
+++ researchTool/test/com/biomatters/plugins/barcoding/validator/research/BarcodeValidatorTest.java
@@ -5,10 +5,9 @@ import org.junit.Test;
 
 /**
  * @author Gen Li
- *         Created on 20/08/14 4:26 PM
+ *         Created on 20/08/14 4:55 PM
  */
 public class BarcodeValidatorTest extends Assert {
-
     @Test
     public void testNameIsCorrect() {
         assertEquals("Barcode Validator", new BarcodeValidatorPlugin().getName());
