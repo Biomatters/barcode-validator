7b069989eeca2331c3f5b9a0ba0989382d81a67e
Matthew Cheung <matthew@biomatters.com>
2014-10-03 14:33:30 +1300

    BV-38 - Test v0.1 of the plugin and send to CBOL Set version

--- researchTool/src/com/biomatters/plugins/barcoding/validator/research/BarcodeValidatorPlugin.java
+++ researchTool/src/com/biomatters/plugins/barcoding/validator/research/BarcodeValidatorPlugin.java
@@ -10,7 +10,7 @@ import com.biomatters.plugins.barcoding.validator.research.report.ValidationRepo
  *         Created on 20/08/14 3:06 PM
  */
 public class BarcodeValidatorPlugin extends GeneiousPlugin {
-    private static final String PLUGIN_VERSION = "0.0.0";
+    private static final String PLUGIN_VERSION = "0.1.0";
 
     @Override
     public String getName() {
