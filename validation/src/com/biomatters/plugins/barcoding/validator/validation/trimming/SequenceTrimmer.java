bde4dee647146a573ca36b258ad5d11828baa6e7
Matthew Cheung <matthew@biomatters.com>
2014-10-05 23:42:23 +1300

    BV-12 Combine steps Update tests for new code

--- validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
@@ -2,8 +2,6 @@ package com.biomatters.plugins.barcoding.validator.validation.trimming;
 
 import com.biomatters.geneious.publicapi.documents.sequence.NucleotideGraphSequenceDocument;
 import com.biomatters.geneious.publicapi.implementations.SequenceExtractionUtilities;
-import com.biomatters.geneious.publicapi.plugin.DocumentOperationException;
-import com.biomatters.geneious.publicapi.utilities.SequenceUtilities;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +13,6 @@ import java.util.List;
  *         Created on 9/09/14 11:03 AM
  */
 public class SequenceTrimmer {
-    private static final int CHROMATOGRAM_NUCLEOTIDE_STATE_NUMBER_RANGE_SIZE = 4;
 
     private SequenceTrimmer() {
     }
@@ -26,22 +23,14 @@ public class SequenceTrimmer {
      * @param documents Supplied NucleotideGraphSequenceDocuments.
      * @param errorProbabilityLimit Error probability limit.
      * @return Trimmed NucleotideGraphSequenceDocuments.
-     * @throws DocumentOperationException
      */
-    public static List<NucleotideGraphSequenceDocument> trimSequences(List<NucleotideGraphSequenceDocument> documents, double errorProbabilityLimit)
-            throws DocumentOperationException {
+    public static List<NucleotideGraphSequenceDocument> trimSequences(List<NucleotideGraphSequenceDocument> documents, double errorProbabilityLimit) {
         List<NucleotideGraphSequenceDocument> trimmedSequences = new ArrayList<NucleotideGraphSequenceDocument>();
-
-        try {
-            for (NucleotideGraphSequenceDocument sequence : documents) {
-                trimmedSequences.add(
-                        trimNucleotideGraphSequenceDocument(sequence, ErrorProbabilityTrimmer.getTrimmage(sequence, TrimmableEnds.Both, errorProbabilityLimit))
-                );
-            }
-        } catch (DocumentOperationException e) {
-            throw new DocumentOperationException("Could not trim documents: " + e.getMessage(), e);
+        for (NucleotideGraphSequenceDocument sequence : documents) {
+            trimmedSequences.add(
+                    trimNucleotideGraphSequenceDocument(sequence, ErrorProbabilityTrimmer.getTrimmage(sequence, TrimmableEnds.Both, errorProbabilityLimit))
+            );
         }
-
         return trimmedSequences;
     }
 
@@ -51,10 +40,8 @@ public class SequenceTrimmer {
      * @param document Supplied NucleotideGraphSequenceDocument.
      * @param trimmage Region lengths.
      * @return Trimmed sequence.
-     * @throws DocumentOperationException
      */
-    private static NucleotideGraphSequenceDocument trimNucleotideGraphSequenceDocument(NucleotideGraphSequenceDocument document, Trimmage trimmage)
-            throws DocumentOperationException {
+    static NucleotideGraphSequenceDocument trimNucleotideGraphSequenceDocument(NucleotideGraphSequenceDocument document, Trimmage trimmage) {
         int from = Math.max(0, trimmage.trimAtStart - 1);
         int to = Math.max(0, document.getSequenceLength() - trimmage.trimAtEnd);
 
