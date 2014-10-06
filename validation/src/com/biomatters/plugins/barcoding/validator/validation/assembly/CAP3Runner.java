f0de85bd351b1c3ac4081a5f031ec1c88e58e52d
Matthew Cheung <matthew@biomatters.com>
2014-10-07 12:42:36 +1300

    BV-43 - CAP3 doesn't run on Windows out of the box -Remove unnecessary import

--- validation/src/com/biomatters/plugins/barcoding/validator/validation/assembly/CAP3Runner.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/assembly/CAP3Runner.java
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
