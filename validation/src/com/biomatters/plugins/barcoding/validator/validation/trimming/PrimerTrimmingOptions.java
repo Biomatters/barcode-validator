714b3a011c99d82273f45a3da11474d0635d5d0a
Gen Li <gen@biomatters.com>
2014-10-31 18:19:25 +1300

    BV-34 - Add primer trimming
    Added primer selection options.

--- validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/PrimerTrimmingOptions.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/PrimerTrimmingOptions.java
@@ -1,23 +1,45 @@
 package com.biomatters.plugins.barcoding.validator.validation.trimming;
 
+import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
+import com.biomatters.geneious.publicapi.implementations.sequence.OligoSequenceDocument;
+import com.biomatters.geneious.publicapi.plugin.DocumentOperationException;
+import com.biomatters.geneious.publicapi.plugin.DocumentSelectionOption;
 import com.biomatters.geneious.publicapi.plugin.Options;
 import jebl.evolution.align.scores.Scores;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Gen Li
  *         Created on 21/10/14 3:13 PM
  */
 public class PrimerTrimmingOptions extends Options {
-    private static final String GAP_OPEN_PENALTY_OPTION_NAME = "gapOpenPenalty";
-    private static final String GAP_EXTENSION_PENALTY_OPTION_NAME = "gapExtensionPenality";
-    private static final String COST_MATRIX_OPTION_NAME = "costMatrix";
+    private static final String PRIMER_SELECTION_OPTION_NAME      = "primers";
+    private static final String GAP_OPEN_PENALTY_OPTION_NAME      = "gapOpenPenalty";
+    private static final String GAP_EXTENSION_PENALTY_OPTION_NAME = "gapExtensionPenalty";
+    private static final String COST_MATRIX_OPTION_NAME           = "costMatrix";
 
     public PrimerTrimmingOptions(Class cls) {
         super(cls);
 
+        addPrimerSelectionOption();
         addGapOpenPenaltyOptions();
         addGapExtensionPenaltyOptions();
         addCostMatrixOptions();
+    }
+
+    public List<OligoSequenceDocument> getPrimers() throws DocumentOperationException {
+        List<OligoSequenceDocument> primers = new ArrayList<OligoSequenceDocument>();
+
+        for (AnnotatedPluginDocument document : ((DocumentSelectionOption)getOption(PRIMER_SELECTION_OPTION_NAME)).getDocuments()) {
+            if (document.getDocumentClass().isAssignableFrom(OligoSequenceDocument.class)) {
+                primers.add((OligoSequenceDocument)document.getDocument());
+            }
+        }
+
+        return primers;
     }
 
     public Scores getScores() {
@@ -30,6 +52,10 @@ public class PrimerTrimmingOptions extends Options {
 
     public double getGapExtensionPenalty() {
         return ((DoubleOption)getOption(GAP_EXTENSION_PENALTY_OPTION_NAME)).getValue();
+    }
+
+    private void addPrimerSelectionOption() {
+        addPrimerSelectionOption(PRIMER_SELECTION_OPTION_NAME, "Primers", DocumentSelectionOption.FolderOrDocuments.EMPTY, true, Collections.<AnnotatedPluginDocument>emptyList());
     }
 
     private void addCostMatrixOptions() {
--- validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
@@ -1,11 +1,15 @@
 package com.biomatters.plugins.barcoding.validator.validation.trimming;
 
 import com.biomatters.geneious.publicapi.documents.sequence.NucleotideGraphSequenceDocument;
+import com.biomatters.geneious.publicapi.documents.sequence.SequenceAnnotationInterval;
 import com.biomatters.geneious.publicapi.implementations.SequenceExtractionUtilities;
 import com.biomatters.geneious.publicapi.implementations.sequence.OligoSequenceDocument;
+import com.biomatters.geneious.publicapi.utilities.SequenceUtilities;
+import jebl.evolution.align.SmithWatermanLinearSpaceAffine;
+import jebl.evolution.align.scores.Scores;
+import jebl.util.ProgressListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Functionality for trimming sequences. Non-instantiable.
@@ -19,39 +23,115 @@ public class SequenceTrimmer {
     }
 
     /**
-     * Trims NucleotideGraphSequenceDocuments by quality scores using the modified Mott algorithm.  This is the same
+     * Trims sequence by quality scores using the modified Mott algorithm.  This is the same
      * algorithm used in PHRED and is also one of the trimming algorithms available in Geneious.
      *
-     * @param documents {@link NucleotideGraphSequenceDocument}s to trim.
+     * @param sequence Sequence to trim.
      * @param errorProbabilityLimit Error probability limit for the modified mott algorithm.
-     * @return Trimmed {@link com.biomatters.geneious.publicapi.documents.sequence.NucleotideGraphSequenceDocument}s.
+     * @return Trimmed sequence.
      */
-    public static List<NucleotideGraphSequenceDocument> trimSequencesByQuality(List<NucleotideGraphSequenceDocument> documents, double errorProbabilityLimit) {
-        List<NucleotideGraphSequenceDocument> trimmedSequences = new ArrayList<NucleotideGraphSequenceDocument>();
-        for (NucleotideGraphSequenceDocument sequence : documents) {
-            trimmedSequences.add(trimSequenceUsingTrimmage(sequence, ErrorProbabilityTrimmer.getTrimmage(sequence, TrimmableEnds.Both, errorProbabilityLimit)));
-        }
-        return trimmedSequences;
+    public static NucleotideGraphSequenceDocument trimSequenceByQuality(NucleotideGraphSequenceDocument sequence, double errorProbabilityLimit) {
+        return trimSequenceUsingTrimmage(sequence, ErrorProbabilityTrimmer.getTrimmage(sequence, TrimmableEnds.Both, errorProbabilityLimit));
     }
 
-    public static List<NucleotideGraphSequenceDocument> trimSequencesByPrimer(List<NucleotideGraphSequenceDocument> documents, List<OligoSequenceDocument> primers) {
-        return null;
+    public static NucleotideGraphSequenceDocument trimSequenceByPrimer(NucleotideGraphSequenceDocument sequence,
+                                                                       OligoSequenceDocument primer,
+                                                                       float gapOpenPenalty,
+                                                                       float gapExtensionPenalty,
+                                                                       Scores scores,
+                                                                       ProgressListener progressListener) {
+        return trimSequenceUsingTrimmage(sequence, getTrimmageForPrimerTrimming(sequence, primer, gapOpenPenalty, gapExtensionPenalty, scores, progressListener));
     }
 
-    public static NucleotideGraphSequenceDocument trimSequenceByPrimer(NucleotideGraphSequenceDocument document, OligoSequenceDocument primer) {
-        return null;
+    public static NucleotideGraphSequenceDocument trimSequenceByQualityAndPrimer(NucleotideGraphSequenceDocument sequence,
+                                                                                 double errorProbabilityLimit,
+                                                                                 OligoSequenceDocument primer,
+                                                                                 float gapOpenPenalty,
+                                                                                 float gapExtensionPenalty,
+                                                                                 Scores scores,
+                                                                                 ProgressListener progressListener) {
+        return trimSequenceUsingTrimmage(sequence,
+                                         max(ErrorProbabilityTrimmer.getTrimmage(sequence, TrimmableEnds.Both, errorProbabilityLimit),
+                                         getTrimmageForPrimerTrimming(sequence, primer, gapOpenPenalty, gapExtensionPenalty, scores, progressListener)));
     }
 
     /**
-     * @param document {@link NucleotideGraphSequenceDocument} to trim
+     * @param sequence {@link NucleotideGraphSequenceDocument} to trim
      * @param trimmage Region lengths to trim
      * @return Trimmed {@link com.biomatters.geneious.publicapi.documents.sequence.NucleotideGraphSequenceDocument}
      */
-    static NucleotideGraphSequenceDocument trimSequenceUsingTrimmage(NucleotideGraphSequenceDocument document, Trimmage trimmage) {
+    static NucleotideGraphSequenceDocument trimSequenceUsingTrimmage(NucleotideGraphSequenceDocument sequence, Trimmage trimmage) {
         SequenceExtractionUtilities.ExtractionOptions options = new SequenceExtractionUtilities.ExtractionOptions(
-                trimmage.getNonTrimmedInterval(document.getSequenceLength()));
-        options.setOverrideName(document.getName() + " trimmed");
+                trimmage.getNonTrimmedInterval(sequence.getSequenceLength()));
+        options.setOverrideName(sequence.getName() + " trimmed");
 
-        return (NucleotideGraphSequenceDocument)SequenceExtractionUtilities.extract(document, options);
+        return (NucleotideGraphSequenceDocument)SequenceExtractionUtilities.extract(sequence, options);
+    }
+
+    private static Trimmage max(Trimmage trimmageOne, Trimmage trimmageTwo) {
+        return new Trimmage(Math.max(trimmageOne.trimAtStart, trimmageTwo.trimAtStart), Math.max(trimmageTwo.trimAtEnd, trimmageTwo.trimAtEnd));
+    }
+
+    /**
+     * Returns trimmage used for primer trimming.
+     *
+     * @param sequence Sequence.
+     * @param primer Primer.
+     * @return Trimmage.
+     */
+    private static Trimmage getTrimmageForPrimerTrimming(NucleotideGraphSequenceDocument sequence,
+                                                         OligoSequenceDocument primer,
+                                                         float gapOpenPenalty,
+                                                         float gapExtensionPenalty,
+                                                         Scores scores,
+                                                         ProgressListener progressListener) {
+        CharSequence traceSequence = sequence.getCharSequence();
+        CharSequence primerSequence = primer.getBindingSequence();
+        CharSequence primerSequenceReversed = SequenceUtilities.reverseComplement(primerSequence);
+
+        Scores scoresWithAdditionalCharacters = getScoresWithAdditionalCharacters(scores, Arrays.asList(SequenceUtilities.removeGaps(traceSequence), SequenceUtilities.removeGaps(primerSequence)));
+
+        SequenceAnnotationInterval leftTrimInterval = new SmithWaterman(new String[] { traceSequence.toString(), primerSequence.toString() },
+                                                                        progressListener,
+                                                                        new SmithWatermanLinearSpaceAffine(scoresWithAdditionalCharacters, gapOpenPenalty, gapExtensionPenalty)).getIntervals()[0];
+        SequenceAnnotationInterval rightTrimInterval = new SmithWaterman(new String[] { traceSequence.toString(), primerSequenceReversed.toString() },
+                                                                         progressListener,
+                                                                         new SmithWatermanLinearSpaceAffine(scoresWithAdditionalCharacters, gapOpenPenalty, gapExtensionPenalty)).getIntervals()[0];
+
+        return new Trimmage(leftTrimInterval.getTo(), sequence.getSequenceLength() - rightTrimInterval.getFrom() + 1);
+    }
+
+    private static Scores getScoresWithAdditionalCharacters(Scores scores, List<CharSequence> sequences) {
+        return Scores.includeAdditionalCharacters(scores, toString(getCharacters(sequences)));
+    }
+
+    private static Set<Character> getCharacters(List<CharSequence> sequences) {
+        Set<Character> characters = new HashSet<Character>();
+
+        for (CharSequence sequence : sequences) {
+            characters.addAll(getCharacters(sequence));
+        }
+
+        return characters;
+    }
+
+    private static Set<Character> getCharacters(CharSequence sequence) {
+        Set<Character> characters = new HashSet<Character>();
+
+        for (int i = 0; i < sequence.length(); i++) {
+            characters.add(sequence.charAt(i));
+        }
+
+        return characters;
+    }
+
+    private static String toString(Set<Character> characters) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Character c : characters) {
+            stringBuilder.append(c);
+        }
+
+        return stringBuilder.toString();
     }
 }
