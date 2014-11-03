4f0b049f1653bfd4cce7b0af5da036f927e5ad5e
Gen Li <gen@biomatters.com>
2014-11-03 14:09:42 +1300

    BV-31 - Allow the user to batch perform the validation pipeline with a range of parameters
    Added handling of trim end overlap..

--- validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/trimming/SequenceTrimmer.java
@@ -56,7 +56,13 @@ public class SequenceTrimmer {
             trimmages.add(getTrimmageForPrimerTrimming(sequence, primer, gapOpenPenalty, gapExtensionPenalty, scores));
         }
 
-        return trimSequenceUsingTrimmage(sequence, max(trimmages));
+        Trimmage maxTrimmage = max(trimmages);
+
+        if (maxTrimmage.trimAtStart > sequence.getSequenceLength() - maxTrimmage.trimAtEnd + 1) {
+            maxTrimmage = new Trimmage(sequence.getSequenceLength(), 0);
+        }
+
+        return trimSequenceUsingTrimmage(sequence, maxTrimmage);
     }
 
     /**
@@ -72,7 +78,7 @@ public class SequenceTrimmer {
     }
 
     private static Trimmage max(Trimmage trimmageOne, Trimmage trimmageTwo) {
-        return new Trimmage(Math.max(trimmageOne.trimAtStart, trimmageTwo.trimAtStart), Math.max(trimmageTwo.trimAtEnd, trimmageTwo.trimAtEnd));
+        return new Trimmage(Math.max(trimmageOne.trimAtStart, trimmageTwo.trimAtStart), Math.max(trimmageOne.trimAtEnd, trimmageTwo.trimAtEnd));
     }
 
     private static Trimmage max(Collection<Trimmage> trimmages) {
@@ -110,6 +116,8 @@ public class SequenceTrimmer {
         SequenceAnnotationInterval rightTrimInterval = new SmithWaterman(new String[] { traceSequence.toString(), primerSequenceReversed.toString() },
                                                                          ProgressListener.EMPTY,
                                                                          new SmithWatermanLinearSpaceAffine(scoresWithAdditionalCharacters, gapOpenPenalty, gapExtensionPenalty)).getIntervals()[0];
+
+
 
         return new Trimmage(leftTrimInterval.getTo(), sequence.getSequenceLength() - rightTrimInterval.getFrom() + 1);
     }
