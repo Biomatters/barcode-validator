3fdd0d611cd598603177115bee5603f0e8697484
Matthew Cheung <matthew@biomatters.com>
2014-11-04 22:34:04 +1300

    BV-44 - Release Barcode Validator 0.2 - Revert hard coding of validation tasks

--- validation/src/com/biomatters/plugins/barcoding/validator/validation/BarcodeValidation.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/BarcodeValidation.java
@@ -4,7 +4,6 @@ import com.biomatters.geneious.publicapi.components.Dialogs;
 import com.biomatters.plugins.barcoding.validator.validation.utilities.ClassUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,6 +20,20 @@ public abstract class BarcodeValidation implements Validation {
      * @return List of BarcodeValidation objects.
      */
     public static synchronized List<BarcodeValidation> getBarcodeValidations() {
-        return Arrays.<BarcodeValidation>asList(new BarcodeConsensusValidation());
+        if (impls == null) {
+            impls = new ArrayList<BarcodeValidation>();
+
+            List<Class> ret = ClassUtils.findClass(IMPLEMENTS_PAKCAGE, new Class[] {BarcodeValidation.class});
+            for (Class cl : ret) {
+                try {
+                    impls.add((BarcodeValidation)cl.newInstance());
+                } catch (InstantiationException e) {
+                    Dialogs.showMessageDialog("Failed to initialize class " + cl.getName(), " because of " + e.getMessage());
+                } catch (IllegalAccessException e) {
+                    Dialogs.showMessageDialog("Failed to access class " + cl.getName(), " because of " + e.getMessage());
+                }
+            }
+        }
+        return impls;
     }
 }
--- validation/src/com/biomatters/plugins/barcoding/validator/validation/TraceValidation.java
+++ validation/src/com/biomatters/plugins/barcoding/validator/validation/TraceValidation.java
@@ -5,7 +5,6 @@ import com.biomatters.geneious.publicapi.documents.sequence.NucleotideGraphSeque
 import com.biomatters.plugins.barcoding.validator.validation.utilities.ClassUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -31,6 +30,21 @@ public abstract class TraceValidation implements Validation {
      * @return List of TraceValidation objects.
      */
     public synchronized static List<TraceValidation> getTraceValidations() {
-        return Arrays.<TraceValidation>asList(new SlidingWindowTraceValidation());
+        if (impls == null) {
+            impls = new ArrayList<TraceValidation>();
+
+            List<Class> ret = ClassUtils.findClass(IMPLEMENTS_PAKCAGE, new Class[]{TraceValidation.class});
+            for (Class cl : ret) {
+                try {
+                    impls.add((TraceValidation)cl.newInstance());
+                } catch (InstantiationException e) {
+                    Dialogs.showMessageDialog("Failed to initialize class " + cl.getName(), " because of " + e.getMessage());
+                } catch (IllegalAccessException e) {
+                    Dialogs.showMessageDialog("Failed to access class " + cl.getName(), " because of " + e.getMessage());
+                }
+            }
+        }
+
+        return impls;
     }
 }
